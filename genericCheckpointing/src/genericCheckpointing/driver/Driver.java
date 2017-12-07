765c8f2771e1c1ea5f6529f3f6d26a7e9122cc7f
Girish <gkurkut1@binghamton.edu>
2017-12-06 23:31:38 -0500

    Initial code written to create proxy

--- genericCheckpointing/src/genericCheckpointing/driver/Driver.java
+++ genericCheckpointing/src/genericCheckpointing/driver/Driver.java
@@ -1,10 +1,30 @@
 package genericCheckpointing.driver;
 
+import genericCheckpointing.server.RestoreI;
+import genericCheckpointing.server.StoreI;
+import genericCheckpointing.server.StoreRestoreI;
+import genericCheckpointing.util.MyAllTypesFirst;
+import genericCheckpointing.util.MyAllTypesSecond;
+import genericCheckpointing.util.ProxyCreator;
+import genericCheckpointing.xmlStoreRestore.StoreRestoreHandler;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ProxyCreator pc = new ProxyCreator();
+		
+		StoreRestoreI cpointRef = (StoreRestoreI) pc.createProxy(
+				 new Class[] {
+				     StoreI.class, RestoreI.class
+				 }, 
+				 new StoreRestoreHandler()
+				 );
+		
+		MyAllTypesFirst myFirst;
+		MyAllTypesSecond  mySecond;
+		
 	}
 
 }
